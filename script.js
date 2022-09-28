9a2b7465b1d3590fc2a6b6aeedc62314098fa80a
totoeevee <76802679+totoeevee@users.noreply.github.com>
2022-09-28 17:14:17 +0000

    made command more thingy

--- script.js
+++ script.js
@@ -11,33 +11,43 @@ const console = [
 ];
 
 function runCommand() {
-  var input = document.getElementById("frm1").value;
-  document.getElementById("frm1").value = "";
-  console.push("totoeevee@Munzgame:~$ " + input)
-  var consoleString = "";
-  for (var i = 0; i < console.length; i++) {
-    if (i != 0) consoleString += "<br>";
-    consoleString += console[i];
-  }
+	var input = document.getElementById("frm1").value;
+	document.getElementById("frm1").value = "";
+	console.push("totoeevee@Munzgame:~$ " + input);
+	for (const command of commands) {
+		if (command.name == input) command.run();
+	}
+	if (!commands.includes(input)) console.push("Unreconized command, use 'help' for available commands.");
+	var consoleString = "";
+	for (var i = 0; i < console.length; i++) {
+		if (i != 0) consoleString += "<br>";
+		consoleString += console[i];
+	}
+	document.getElementById("mainp").innerHTML = consoleString;
 }
 
 class Command {
-  constructor(name, desc, run) {
-    this.name = name;
-    this.desc = desc;
-    this.run = run;
-  }
+	constructor(name, desc, run) {
+		this.name = name;
+		this.desc = desc;
+		this.run = run;
+	}
 }
 
 const M = {};
-M.registerCommand = function (command) {
-  commands.push(command);
-  return this;
+M.registerCommand = function(command) {
+	commands.push(command);
+	return this;
 }
 
 M
-  .registerCommand(new Command())
+	.registerCommand(new Command('help', 'shows all commands', () => {
+		console.push("Available Commands:");
+		for (const command of commands) {
+			console.push(" - " + command.name + ": " + command.desc);
+		}
+	}))
 
 document.addEventListener('keydown', (e) => {
-  if (e.key == "Enter") runCommand();
+	if (e.key == "Enter") runCommand();
 });
